04d6f917d54a9569b470e45988202f44ac4e9bba
dualeali <abdifarhan080@gmail.com>
2025-07-23 10:46:28 +0300

    initial backend commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const cors = require('cors');
+
+// Load environment variables from .env
+dotenv.config();
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log('✅ Connected to MongoDB Atlas'))
+.catch((err) => console.error('❌ MongoDB connection failed:', err));
+
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json()); // Parse JSON requests
+
+// Simple route
+app.get('/', (req, res) => {
+  res.send('🕌 Muslim Prayer Tracker Backend is running');
+});
+
+// Start server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
